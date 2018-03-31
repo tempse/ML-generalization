9157b8ae45eba9c67994426cbf7f1ce8f61672e3
Sebastian Templ <sebastian.templ@cern.ch>
2018-03-31 20:57:38 +0200

    Code cleaning

--- generalization/__init__.py
+++ generalization/__init__.py
@@ -1,9 +1,2 @@
-# import os
-# 
-# if os.environ.get('DISPLAY') == '':
-#     print('No display found. Using non-interactive matplotlib Agg backend.')
-#     import matplotlib
-#     matplotlib.use('Agg')
-
 import matplotlib
 matplotlib.use('Agg')
